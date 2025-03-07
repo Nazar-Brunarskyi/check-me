d8854ebaf8b375e7ca47ff1c7dea8c37fd347664
Nazar-Brunarskyi <3nnazar836@gmail.com>
2025-03-07 20:55:47 +0200

    add console.log

--- apps/check-me-api/src/app/app.service.ts
+++ apps/check-me-api/src/app/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    console.log(123);
+
     return { message: 'Hello API' };
   }
 }
