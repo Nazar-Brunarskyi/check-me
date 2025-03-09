9862d69dac5fa6df1159ddc2f85aee453da68099
Nazar-Brunarskyi <3nnazar836@gmail.com>
2025-03-09 22:01:18 +0200

    test deployment of whole application

--- apps/check-me-api/src/app/app.controller.ts
+++ apps/check-me-api/src/app/app.controller.ts
@@ -4,7 +4,7 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   get() {
-    return { status: 'OK' };
+    return { status: 'OK', message: 'Check Me API' };
   }
 
   @Get('health')
