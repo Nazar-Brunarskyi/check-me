c12b7132bd893e10988c2fec002d0a60cd29c2e6
Nazar-Brunarskyi <3nnazar836@gmail.com>
2025-03-10 22:16:12 +0200

    add primeng

--- apps/check-me-web/src/app/app.config.ts
+++ apps/check-me-web/src/app/app.config.ts
@@ -1,7 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import { theme } from '../theme';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes)],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: theme,
+      },
+    }),
+  ],
 };
