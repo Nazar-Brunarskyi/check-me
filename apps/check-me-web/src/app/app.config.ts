3be6e29d142d6b70ec8b7ad393f0b6eb5b6ac126
Nazar-Brunarskyi <3nnazar836@gmail.com>
2025-03-13 23:44:12 +0200

    add alias and tegs

--- apps/check-me-web/src/app/app.config.ts
+++ apps/check-me-web/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { getAngularThemeProviders } from '@check-me/angular-theme/theme/get-angular-theme-providers';
+import { getAngularThemeProviders } from '@check-me/angular-theme';
+// import { getAngularThemeProviders } from '@check-me/angular-theme/theme/get-angular-theme-providers';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
---
+++ libs/angular-theme/src/index.ts
@@ -0,0 +1,1 @@
+export * from './theme/get-angular-theme-providers'
