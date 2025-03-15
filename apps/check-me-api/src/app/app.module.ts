fd276010fe61537243575a688dccfa6c78900685
Nazar-Brunarskyi <3nnazar836@gmail.com>
2025-03-15 18:12:06 +0200

    add nestjs/config

--- apps/check-me-api/src/app/app.module.ts
+++ apps/check-me-api/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { dbConfiguration } from '../common/environment/configuration';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfiguration],
+    }),
+  ],
   providers: [],
   controllers: [AppController],
 })
---
+++ apps/check-me-api/src/common/environment/configuration.ts
@@ -0,0 +1,5 @@
+export const dbConfiguration = () => ({
+  database: {
+    uri: process.env.MONGODB_URI,
+  },
+});
