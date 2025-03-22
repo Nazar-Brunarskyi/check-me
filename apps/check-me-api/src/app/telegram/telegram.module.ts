cc1a35e00aeebf16afa73932ddc6edd693889817
Nazar-Brunarskyi <3nnazar836@gmail.com>
2025-03-23 00:04:55 +0200

    chore: Remove deprecated root command service and update module imports

--- apps/check-me-api/src/app/telegram/telegram.module.ts
+++ apps/check-me-api/src/app/telegram/telegram.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { DiscoveryService } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TelegramController } from './controllers/telegram.controller';
-import { RootCommandService } from './routes/root-command.service';
+import { RootCommandService } from './routers/root-command.service';
 import { TelegramCommunicationService } from './services/telegram-communication.service';
 import { TelegramService } from './services/telegram.service';
 
