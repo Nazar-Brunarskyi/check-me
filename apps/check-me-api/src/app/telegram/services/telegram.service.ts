215a03df9fc4a36a638d40c474074de0cc9aafa4
Nazar-Brunarskyi <3nnazar836@gmail.com>
2025-03-22 19:27:22 +0200

    format project

--- apps/check-me-api/src/app/telegram/services/telegram.service.ts
+++ apps/check-me-api/src/app/telegram/services/telegram.service.ts
@@ -1,9 +1,10 @@
 import { ITelegramUpdate } from '@check-me/models';
 import { Injectable } from '@nestjs/common';
+
 @Injectable()
 export class TelegramService {
   async handleWebhook(data: ITelegramUpdate): Promise<void> {
-    console.log(data.message.text);
+    console.log(data);
 
     return;
   }
