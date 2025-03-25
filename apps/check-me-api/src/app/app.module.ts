ada2f542d9dfc299a01f40651fb6c43420160e86
Nazar-Brunarskyi <3nnazar836@gmail.com>
2025-03-25 20:46:07 +0200

    chore: Add UsersModule for managing users

--- apps/check-me-api/src/app/app.module.ts
+++ apps/check-me-api/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { TelegramModule } from './telegram/telegram.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TelegramModule } from './telegram/telegram.module';
     }),
     TelegramModule,
     AuthModule,
+    UsersModule,
   ],
   providers: [],
   controllers: [AppController],
