67af0d1c1061d68e368c3e85335a2f3780e15eb1
Nazar-Brunarskyi <3nnazar836@gmail.com>
2025-03-26 17:07:01 +0200

    feat: Add login route and component

--- apps/check-me-web/src/app/app.routes.ts
+++ apps/check-me-web/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Route } from '@angular/router';
+import { LoginPageComponent } from '../routes/login-page/login-page.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent, pathMatch: 'full' },
+];
