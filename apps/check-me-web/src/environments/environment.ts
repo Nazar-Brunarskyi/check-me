b2bc10482bb296ce1bf51db0a85cbfc516f2c073
Nazar-Brunarskyi <3nnazar836@gmail.com>
2025-03-30 19:08:57 +0300

    chore: Update API URL in environment.ts file

--- apps/check-me-web/src/environments/environment.ts
+++ apps/check-me-web/src/environments/environment.ts
@@ -1,6 +1,5 @@
 import { IAngularEnvironment } from '@check-me/models';
 
 export const environment: IAngularEnvironment = {
-  apiUrl: 'https://lkmjt1p9-3000.euw.devtunnels.ms',
-  // apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3000',
 };
